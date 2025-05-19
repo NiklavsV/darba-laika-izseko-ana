a73b093fa74ec4d699359dea8b49220f65d335b7
NiklavsV <niklavs.veiksans@edu.rtu.lv>
2025-05-19 21:41:22 +0300

    izvada laiku un datumu kad tika nospiesta poga f13

--- Main.py
+++ Main.py
@@ -1,20 +1,26 @@
 
 
 import keyboard
-import datetime
+from datetime import *
 
 class Main:
+
     def __init__(self):
         pass
 
     def on_f13_press(self, event):
+        onn = False
         if event.name == 'f13':
+            now = datetime.now()
+            evtime = now.strftime("%Y/%m/%d %H:%M")
             print("f13 key was pressed!")
+            onn = not onn
+            if onn == True:
+                print(evtime)
 
     def start(self):
         print("Press f13 to detect. Press ESC to stop.")
         keyboard.on_press(self.on_f13_press)
-
         keyboard.wait('esc')
 
     def stop(self):
