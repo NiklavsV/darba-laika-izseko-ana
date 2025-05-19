30afef009ec055e9441d728bdde938dd0cc80d49
NiklavsV <niklavs.veiksans@edu.rtu.lv>
2025-05-19 21:14:52 +0300

    iznemu nevajadzigas rindas

--- Main.py
+++ Main.py
@@ -5,21 +5,19 @@ import datetime
 
 class Main:
     def __init__(self):
-        self.running = False
+        pass
 
     def on_f13_press(self, event):
         if event.name == 'f13':
             print("f13 key was pressed!")
 
     def start(self):
-        self.running = True
         print("Press f13 to detect. Press ESC to stop.")
         keyboard.on_press(self.on_f13_press)
 
         keyboard.wait('esc')
 
     def stop(self):
-        self.running = False
         keyboard.unhook_all()
         print("Stopped detecting key presses.")
 
